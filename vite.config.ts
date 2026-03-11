3b5fe667e2db7ce235c69f469b10c991193db387
akunkel <akunkel23@gmail.com>
2026-03-11 12:05:11 -0700

    Updated deployment config for Cloudflare Pages.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import checker from 'vite-plugin-checker';
+import { cloudflare } from '@cloudflare/vite-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
+        cloudflare(),
         react(),
         checker({
             typescript: true,
